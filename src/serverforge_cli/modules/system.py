62f4193903b35263d75cb5275ba6d01a69bdc2e0
Manuel <manuel.garcia.rodriguez.010@gmail.com>
2026-08-30 08:34:50 +0200

    fix: Manage sudo in system module correctly

--- src/serverforge_cli/modules/system.py
+++ src/serverforge_cli/modules/system.py
@@ -1,13 +1,12 @@
 from serverforge_cli.utils import run_command
 
 def update_system():
-    run_command(["apt", "update"])
+    run_command(["apt", "update"], requires_sudo=True)
 
     
 def upgrade_system():
-    run_command(["apt", "upgrade", "-y"])
+    run_command(["apt", "upgrade", "-y"], requires_sudo=True)
 
 
 def full_upgrade_system():
-    run_command(["apt", "full-upgrade", "-y"])
-
+    run_command(["apt", "full-upgrade", "-y"], requires_sudo=True)
